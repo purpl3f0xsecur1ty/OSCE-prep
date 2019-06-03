19db77f5711243ea0ddcf26b4e1b26ab200c0253
purpl3-f0x <42351007+purpl3-f0x@users.noreply.github.com>
2019-06-02 22:05:44 -0400

    Update gmon_fuzz.py

--- gmon_fuzz.py
+++ gmon_fuzz.py
@@ -2,20 +2,20 @@
 
 from boofuzz import *
 
-host = '10.0.0.130'	# Windows XP target
-port = 9999			# VulnServer port
+host = '10.0.0.130'    # Windows XP target
+port = 9999            # VulnServer port
 
 def main():
 	session = Session(target = Target(connection = SocketConnection(host, port, proto='tcp')))
 	
-	s_initialize("GMON")	# Session name
+	s_initialize("GMON")    # Session name
 	
-	s_string("GMON", fuzzable = False)	# These strings are fuzzqable by default, so here instead of blank, we specify 'false'
-	s_delim(" ", fuzzable = False)		# We don't want to fuzz the space between "GMON" and our arg
-	s_string("FUZZ")					# This value is arbitrary as we did not specify 'False' for fuzzing. Boofuzz will fuzz this string now.
+	s_string("GMON", fuzzable = False)    # These strings are fuzzqable by default, so here instead of blank, we specify 'false'
+	s_delim(" ", fuzzable = False)        # We don't want to fuzz the space between "GMON" and our arg
+	s_string("FUZZ")                      # This value is arbitrary as we did not specify 'False' for fuzzing. Boofuzz will fuzz this string now.
 	
-	session.connect(s_get("GMON"))		# Having our 'session' variable connect following the guidelines established in "GMON"
-	session.fuzz()						# Calling this function actually performs the fuzzing
+	session.connect(s_get("GMON"))        # Having our 'session' variable connect following the guidelines established in "GMON"
+	session.fuzz()                        # Calling this function actually performs the fuzzing
 
 if __name__ == "__main__":
 	main()
